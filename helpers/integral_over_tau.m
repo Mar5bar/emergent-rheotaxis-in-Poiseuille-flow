238871e6d893944674d0b4bdbae6b6c4ec149f0e
Benjamin Walker <benjaminwalker@blueyonder.co.uk>
2022-02-23 12:15:24 +0000

    The integral over tau is now computed systematically when H = H0Thresh, which has formally infinite period.

--- helpers/integral_over_tau.m
+++ helpers/integral_over_tau.m
@@ -1,4 +1,4 @@
-function res = integral_over_tau(H0, params)
+function [res, period] = integral_over_tau(H0, params)
 % Numerically compute the required integral over a period, as a function of H0.
 
     % The integration will depend structurally on whether or not H0 is above
@@ -11,9 +11,9 @@ function res = integral_over_tau(H0, params)
 
     % We need to compute theta0 and z0 as functions of time.
     if H0 <= thresh
-        % We'll go from z0 = 0, theta = thetaMin to z0 = 0, theta = thetaMax.
-        thetaMin = gInv(H0, params);
-        initCond = [0;thetaMin];
+        % We'll go from z0 = z0Init, theta = pi to z0 = 0, theta = thetaMax.
+        z0Init = z0_fun(pi, H0, params);
+        initCond = [z0Init; pi];
         % Use z0 = 0 as the termination condition.
         eventFun = @lessThanThreshEventFnc;
     else
@@ -47,63 +47,29 @@ function res = integral_over_tau(H0, params)
     integrand = dH0_integrand_dtau(z0, theta0, params);
     res = trapz(taus, integrand);
 
-    % We'll need to double this if H0 <= threshold. Note that the period also
-    % doubles.
+    % If H0 <= thresh, we only computed period/4, so scale up. This won't
+    % alter the computed result.
     if H0 <= thresh
-        res = 2 * res;
-        period = 2 * period;
+        res = 4 * res;
+        period = 4 * period;
     end
 
     % Finally, divide by the period, as this should be an average.
     res = res / period;
 
 end
-% for Hamiltonian theta0. We will compute the appropriate domain for theta0
-% and assume that fun(z0,theta0) = - fun(-z0, theta0, params).
-
-% Due to these assumptions, we will take z0 >= 0 always when computing the
-% integral, corresponding to the upper branch of the solution (z0, theta0). On
-% this branch, theta0 is increasing in time, so we integrate from smallest
-% theta0 to biggest theta0. If H0 is larger than the threshold H0Thresh, then
-% the trajectory does not loop and we simply integrate. If H0 is smaller than
-% the threshold, then we must double our computed integral to account for both
-% halves of the loop (noting the antisymmetry of the integrand).
-%     thresh = H0_thresh(params);
-
-%     if H0 <= thresh
-%         % The range of theta for any H0 less than H0Thresh is given by gInv(H0) and 2*pi - gInv(H0).
-%         thetaMin = gInv(H0, params);
-%         thetaMax = 2*pi - thetaMin;
-%     else
-%         % The range of theta is [0,2*pi]
-%         thetaMin = 0;
-%         thetaMax = 2*pi;
-%     end
-
-%     % Integrate over the range of thetas.
-%     thetas = linspace(thetaMin,thetaMax,1e4);
-%     res = trapz(thetas, fun(z0_fun(thetas, H0, params), thetas, params));
-
-%     if H0 <= thresh
-%         % Double the result, as we go over two branches in a loop.
-%         % Here, we exploit the oddness of fun in z0.
-%         res = 2 * res;
-%     end
-
-% end
-
 
 function [value, isterminal, direction] = lessThanThreshEventFnc(t,y)
-    % Terminate when z0 hits 0.
-    value = y(1);
+    % Terminate when z0 hits 0, to 4 dp.
+    value = round(y(1),4);
     isterminal = 1;
     % z0 will hit 0 from above.
     direction = -1;
 end
 
 function [value, isterminal, direction] = moreThanThreshEventFnc(t,y)
-    % Terminate when theta0 hits 2*pi.
-    value = y(2) - 2*pi;
+    % Terminate when theta0 hits 2*pi, to 4 dp.
+    value = round(y(2) - 2*pi,4);
     isterminal = 1;
     % theta0 will hit 0 from below.
     direction = 1;
